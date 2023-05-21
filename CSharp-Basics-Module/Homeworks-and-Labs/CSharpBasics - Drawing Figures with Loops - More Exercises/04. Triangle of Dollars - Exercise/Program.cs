702b74ebd2d95f6e68019d18408ccc3430763361
rumenvasil3v <rumenstvass@gmail.com>
2023-05-21 17:12:14 +0300

    Adding additional exercises form Drawing Figures with Loops

---
+++ CSharp-Basics-Module/Homeworks-and-Labs/CSharpBasics - Drawing Figures with Loops - More Exercises/04. Triangle of Dollars - Exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace _04._Triangle_of_Dollars___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int columns = 1; columns <= rows; columns++)
+                {
+                    Console.Write("$ ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
