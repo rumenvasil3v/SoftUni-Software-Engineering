07cc28788fa9775314fe59427cd19c5a58ef3131
rumenvasil3v <rumenstvass@gmail.com>
2023-05-21 16:25:04 +0300

    Added exercises from First Steps in Coding - Exercise

---
+++ CSharp-Basics-Module/Homeworks-and-Labs/01.CSharp-First-Steps-In-Coding/CSharp-First-Steps-In-Coding-Exercise/04. Vacation Books List/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04._Vacation_Books_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. We read from the console number pages for one book.
+            int pages = int.Parse(Console.ReadLine());
+            //2.We read from the console pages per hour that Joro read. 
+            int pagesHour = int.Parse(Console.ReadLine());
+            //
+            int daysForBook = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(pages / pagesHour /daysForBook );
+        }
+    }
+}
---
+++ CSharp-Basics-Module/Homeworks-and-Labs/01.CSharp-First-Steps-In-Coding/CSharp-First-Steps-In-Coding-Exercise/demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The numbers, which we read are: {num + num2} ");
+        }
+    }
+}
