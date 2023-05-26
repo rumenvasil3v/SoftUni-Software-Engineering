0d3b18d2ae82f9b5d9fbda9eb424a0f649e3fef9
rumenvasil3v <rumenstvass@gmail.com>
2023-05-27 00:22:27 +0300

    Adding arrays - lab

---
+++ CSharp-Fundamentals-Module/Homeworks-and-Labs/03.CSharpFund - Arrays/CSharpFundametals - Lab/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] reverseNumbers = new int[n];
+
+            for (int index = 0; index < reverseNumbers.Length; index++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+
+                reverseNumbers[index] = numbers;
+            }
+
+            for (int index = reverseNumbers.Length; index > 0; index--)
+            {
+                Console.Write(reverseNumbers[index - 1] + " ");
+            }
+        }
+    }
+}
---
+++ CSharp-Fundamentals-Module/Homeworks-and-Labs/03.CSharpFund - Arrays/CSharpFundametals - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] randomStrings = Console.ReadLine().Split(" ");
+
+            for (int index = 0; index < randomStrings.Length / 2; index++)
+            {
+                string firstIndex = randomStrings[index];
+                randomStrings[index] = randomStrings[randomStrings.Length - 1 - index];
+                randomStrings[randomStrings.Length - 1 - index] = firstIndex;
+            }
+
+            Console.WriteLine(string.Join(' ', randomStrings));
+        }
+    }
+}
