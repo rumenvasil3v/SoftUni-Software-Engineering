646225a20af0f646f43eba5668d9621bf9f4a596
rumenvasil3v <rumenstvass@gmail.com>
2023-05-31 23:22:18 +0300

    Adding files

---
+++ CSharp-Fundamentals-Module/Homeworks-and-Labs/04.CSharpFund - Methods/CSharpFundamentals -Methods-Lab/demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void PrintHeader(string name)
+        {
+            Console.WriteLine("==============Header================");
+        }
+
+        static void PrintFooter(string name)
+        {
+            Console.WriteLine($"");
+        }
+    }
+}
