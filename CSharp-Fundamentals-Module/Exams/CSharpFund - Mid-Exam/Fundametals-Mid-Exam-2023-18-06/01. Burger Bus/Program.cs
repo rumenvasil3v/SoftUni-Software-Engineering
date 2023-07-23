da5688c6b16aceb878f6377330e5154a4dcc0ccc
rumenvasil3v <rumenstvass@gmail.com>
2023-07-23 03:57:45 +0300

    Adding Fund mid exam

---
+++ CSharp-Fundamentals-Module/Exams/CSharpFund - Mid-Exam/Fundametals-Mid-Exam-2023-18-06/01. Burger Bus/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _01._Burger_Bus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCities = int.Parse(Console.ReadLine());
+            decimal totalProfit = 0.0m;
+
+            for (int i = 1; i <= numberOfCities; i++)
+            {
+                decimal profit = 0.0m;
+
+                string nameOfCity = Console.ReadLine();
+                decimal moneyEarned = decimal.Parse(Console.ReadLine());
+                decimal ownerExpenses = decimal.Parse(Console.ReadLine());
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    totalProfit = EveryFifthCity(nameOfCity, moneyEarned, ownerExpenses, profit, totalProfit);
+                }
+                else if (i % 3 == 0)
+                {
+                    totalProfit = EveryThirdCity(nameOfCity, moneyEarned, ownerExpenses, totalProfit, profit);
+                }
+                else if (i % 5 == 0)
+                {
+                    totalProfit = EveryFifthCity(nameOfCity, moneyEarned, ownerExpenses, profit, totalProfit);
+                }
+                else
+                {
+                    totalProfit = TheOtherCities(nameOfCity, moneyEarned, ownerExpenses, profit, totalProfit);
+                }
+            }
+
+            PrintTotalProfit(totalProfit);
+        }
+
+        static decimal EveryThirdCity(string nameOfCity, decimal moneyEarned, decimal ownerExpenses, decimal totalProfit, decimal profit)
+        {
+            ownerExpenses += ownerExpenses * 0.5m;
+            profit = moneyEarned - ownerExpenses;
+            totalProfit += profit;
+            Console.WriteLine($"In {nameOfCity} Burger Bus earned {profit:f2} leva.");
+            return totalProfit;
+        }
+        
+        static decimal EveryFifthCity(string nameOfCity, decimal moneyEarned, decimal ownerExpenses, decimal profit, decimal totalProfit)
+        {
+            profit = moneyEarned * 0.9m - ownerExpenses;
+            totalProfit += profit;
+            Console.WriteLine($"In {nameOfCity} Burger Bus earned {profit:f2} leva.");
+            return totalProfit;
+        }
+        
+        static decimal TheOtherCities(string nameOfCity, decimal moneyEarned, decimal ownerExpenses, decimal profit, decimal totalProfit)
+        {
+            profit = moneyEarned - ownerExpenses;
+            totalProfit += profit;
+            Console.WriteLine($"In {nameOfCity} Burger Bus earned {profit:f2} leva.");
+            return totalProfit;
+        }
+        
+        static void PrintTotalProfit(decimal totalProfit)
+        {
+            Console.WriteLine($"Burger Bus total profit: {totalProfit:f2} leva.");
+        }
+    }
+}
---
+++ CSharp-Fundamentals-Module/Exams/CSharpFund - Mid-Exam/Fundametals-Mid-Exam-2023-18-06/Problem 3/Program.cs
@@ -0,0 +1,100 @@
+﻿
+/*
+Ace of Diamonds, Queen of Hearts, King of Clubs
+3
+Add, King of Diamonds
+Insert, 2, Jack of Spades
+Remove, Ace of Diamonds
+
+Two of Clubs, King of Spades, Five of Spades, Jack of Hearts
+2
+Add, Two of Clubs
+Remove, Five of Hearts
+
+Jack of Spades, Ace of Clubs, Jack of Clubs
+2
+Insert, -1, Queen of Spades
+Remove At, 1
+ */
+namespace Problem_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cards = Console
+                .ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                string commands = Console.ReadLine();
+                string[] arguments = commands.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                switch (arguments[0])
+                {
+                    case "Add":
+                        string cardName = arguments[1];
+                        if (cards.Contains(cardName))
+                        {
+                            Console.WriteLine("Card is already in the deck");
+                        }
+                        else
+                        {
+                            cards.Add(cardName);
+                            Console.WriteLine("Card successfully added");
+                        }
+                        break;
+                    case "Remove":
+                        string removeCardName = arguments[1];
+                        if (!cards.Contains(removeCardName))
+                        {
+                            Console.WriteLine("Card not found");
+                        }
+                        else
+                        {
+                            cards.Remove(removeCardName);
+                            Console.WriteLine("Card successfully removed");
+                        }
+                        break;
+                    case "Remove At":
+                        int removeAtIndexCard = int.Parse(arguments[1]);
+                        if (removeAtIndexCard >= 0 && removeAtIndexCard < cards.Count)
+                        {
+                            cards.RemoveAt(removeAtIndexCard);
+                            Console.WriteLine("Card successfully removed");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Index out of range");
+                        }
+                        break;
+                    case "Insert":
+                        int insertAtIndex = int.Parse(arguments[1]);
+                        string insertCardName = arguments[2];
+                        if (insertAtIndex >= 0 && insertAtIndex < cards.Count)
+                        {
+                            if (cards.Contains(insertCardName))
+                            {
+                                Console.WriteLine("Card is already added");
+                            }
+                            else
+                            {
+                                cards.Insert(insertAtIndex, insertCardName);
+                                Console.WriteLine("Card successfully added");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Index out of range");
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", cards));
+        }
+    }
+}
